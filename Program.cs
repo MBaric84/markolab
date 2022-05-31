6aa9769a3826a2c6c6ca13406d6f8a71ee8f49d9
s <s>
2022-05-31 17:05:02 +0200

    evo

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace markolab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!llll");
         }
     }
 }
