90d7c4427243507772058ae60445cfcad250b3b1
Marino Jurić <73504349+marinojuric@users.noreply.github.com>
2022-05-31 17:12:04 +0200

    Promjena

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace markolab
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!llll");
+            Console.WriteLine("Marino");
         }
     }
 }
